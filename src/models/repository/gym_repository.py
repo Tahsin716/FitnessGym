01d1c4b0c1f83c37c7a142f7b24488f232f93e53
Tahsin Rashad <tahsin.rashad716@gmail.com>
2024-12-01 12:50:36 +0000

    security exception added

--- src/models/repository/gym_repository.py
+++ src/models/repository/gym_repository.py
@@ -2,6 +2,7 @@ import logging
 from typing import Tuple
 
 from src.models.entities.gym import Gym
+from src.models.exception.security_exception import SecurityException
 
 
 class GymRepository:
@@ -11,24 +12,27 @@ class GymRepository:
     def create_gym(self, data : dict) -> Tuple[bool, str, Gym | None]:
         try:
             if data is None:
-                raise Exception("")
+                raise SecurityException("data cannot be empty")
 
             if data['post_code'] is None or len(data['post_code']) == 0:
-                raise Exception("")
+                raise Exception("post code cannot be empty")
 
             if data['address'] is None or len(data['address']) == 0:
-                raise Exception("")
+                raise Exception("address cannot be empty")
 
             if data['contact_number'] is None or len(data['contact_number']) == 0:
-                raise Exception("")
+                raise Exception("invalid contact number")
 
             if data['email'] is None or len(data['email']) == 0:
-                raise Exception("")
+                raise Exception("invalid email")
 
             gym = Gym({})
             self.__gyms.append(gym)
 
             return True, "", gym
+        except SecurityException as e:
+            logging.error(f"SecurityException occurred while saving gym: {str(e)}")
+            return False, str(e), None
         except Exception as e:
             logging.error(f"Error occurred while saving gym: {str(e)}")
             return False, str(e), None
